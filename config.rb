fae9fa3fc091d12f8927dc7ca580bd764ae5d8c4
Ubuntu <ubuntu@ip-10-85-193-17.eu-west-2.compute.internal>
2021-04-21 19:24:05 +0000

    changed paths

--- config.rb
+++ config.rb
@@ -6,3 +6,12 @@ activate :deploy do |deploy|
   deploy.deploy_method = :git
   deploy.branch   = 'develop' # default: gh-pages
 end
+
+configure :build do
+  # Generate relative paths to the repo when deploying to GitHub Pages
+  config[:http_prefix] = '/test-actions'
+  config[:css_dir] = ""
+  config[:js_dir] = ""
+  # activate :minify_css
+  # activate :minify_javascript
+end
