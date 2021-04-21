cb8a4fa17baee05d157a173e812d3c40181614d2
Ubuntu <ubuntu@ip-10-85-193-17.eu-west-2.compute.internal>
2021-04-21 19:41:44 +0000

    fixed typo

--- config.rb
+++ config.rb
@@ -10,7 +10,7 @@ end
 configure :build do
   # Generate relative paths to the repo when deploying to GitHub Pages
   config[:http_prefix] = '/test-actions'
-  config[:css_dir] = ""
+  #config[:css_dir] = ""
   config[:js_dir] = ""
   # activate :minify_css
   # activate :minify_javascript
